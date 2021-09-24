42fd9b793662f23778d9e55f3f738720cd304e1b
Yescii <cgonzalez.jesse@gmail.com>
2021-09-24 19:34:46 -0400

    created an express application the required files. View folder to display list of bugets and models folder to store the data

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const budgets = require("./models/budget");
+
+//INDEX
+app.get("/budgets", (req, res) => {
+  res.render("index.ejs", { allBudgets: budgets });
+});
+
+app.get("/budgets/:idxBudget", (req, res) => {
+  res.render("show.ejs", { budget: req.params.idxBudget });
+});
+
+app.listen(port, () => {
+  console.log("hello");
+});
