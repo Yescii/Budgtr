896831299f1185935f3f03751d4de9fe6e373888
Yescii <cgonzalez.jesse@gmail.com>
2021-09-24 21:05:51 -0400

    created a route to form page that require input create a new budget

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const app = express();
 const port = 3000;
 
 const budgets = require("./models/budget");
+const methodOverride = require("method-override");
+
+//Middleware
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+//near the top, around other app.use() calls needed this for req.body to exist
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
 
 //INDEX
 app.get("/budgets", (req, res) => {
   res.render("index.ejs", { allBudgets: budgets });
 });
 
+//route to create a new budget
+app.get("/budgets/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// route to the index of budget
 app.get("/budgets/:idxBudget", (req, res) => {
   res.render("show.ejs", { budget: budgets[req.params.idxBudget] });
 });
