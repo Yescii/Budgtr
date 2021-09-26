2d5664bb48226fc75b19f5db74733c54ed7c209d
Yescii <cgonzalez.jesse@gmail.com>
2021-09-26 17:15:07 -0400

    add a post route to add a new budget, and add some code to Index-get-route to sum the ammouts and pass the the data to index.ejs file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 const budgets = require("./models/budget");
-const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
 
 //Middleware
 app.use((req, res, next) => {
@@ -14,16 +14,39 @@ app.use((req, res, next) => {
 //near the top, around other app.use() calls needed this for req.body to exist
 app.use(express.urlencoded({ extended: false }));
 
-app.use(methodOverride("_method"));
+//parse application
+app.use(bodyParser.urlencoded());
+
+// parse application json
+app.use(bodyParser.json());
 
 //INDEX
 app.get("/budgets", (req, res) => {
-  res.render("index.ejs", { allBudgets: budgets });
+  //sum all amount of budget values from amount key
+  let bankSum = (allBudgets) =>
+    allBudgets.reduce((sum, item) => parseInt(item.amount) + sum, 0);
+  console.log(bankSum(budgets));
+  res.render("index.ejs", {
+    allBudgets: budgets,
+    bankAccount: bankSum(budgets),
+  });
+
+  console.log(budgets);
 });
 
 //NEW route to create a new budget
 app.get("/budgets/new", (req, res) => {
   res.render("new.ejs");
+});
+
+// creats a new budget object stored in req.body
+app.post("/budgets", (req, res) => {
+  // add tags to array by seprating them by space (" ")
+  let arrTags = [];
+  arrTags = String(req.body.tags).split(" ");
+  req.body.tags = arrTags;
+  budgets.push(req.body);
+  res.redirect("/budgets");
 });
 
 // route to the index of budget
